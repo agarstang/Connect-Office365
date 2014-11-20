b6d8dc83b449af6698eae8df2fd97f583f58ebd3
Adam Garstang <adamgarstang@googlemail.com>
2014-11-20 21:58:00 +0000

    Add Connect-Office365.psm1

---
+++ Connect-Office365.psm1
@@ -0,0 +1,17 @@
+﻿
+function Connect-Office365 {
+
+    param(
+    [Parameter(Mandatory=$false)][PSCredential]$UserCredential
+    )
+
+    if(-not($UserCredential)) { $UserCredential = Get-Credential }
+
+    Import-Module MSOnline -Scope Global
+    Connect-MsolService -Credential $UserCredential
+
+    $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Import-Module (Import-PSSession $ExchangeSession -AllowClobber) -Global 
+}
+
+
